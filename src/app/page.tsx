de38c86b1a7d7be39d845f3d9e08384c768f8522
Arshak Asriyan <aasriyan01@gmail.com>
2025-03-23 14:04:25 -0500

    Add logo SVG and update layout and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-rose-500">Hello World</h1>
-      <Button>Click me</Button>
+      <Image src="/logo.svg" alt="logo" width={50} height={50} />
+      <p className="text-xl font-semibold tracking-tight">Copytube</p>
     </div>
   );
 }
